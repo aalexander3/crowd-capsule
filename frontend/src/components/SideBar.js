51bb1b8ead046de43468af19f4c334e3866215e1
github username <frank@thefrankharvey.com>
2018-10-20 18:18:01 -0400

    sidebar WIP

---
+++ frontend/src/components/SideBar.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+import AboutEarth from './AboutEarth';
+import Upload from './Upload';
+
+class SideBar extends Component {
+
+state = {
+}
+
+  render() {
+    console.log('sidebar')
+    return (
+      <div className="sidebar_container">
+        <Link to={ Upload }>Upload</Link>
+        <Link to={ AboutEarth }>About Earth</Link>
+        <Link to={ Map }>Map</Link>
+      </div>
+    );
+  }
+}
+
+export default SideBar;
