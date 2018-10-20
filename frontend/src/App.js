f64168fbfd631080e36a7e8aea9d7fbb7ec81308
MatthewDtotheG <mdgarrett1984@gmail.com>
2018-10-20 16:52:53 -0400

    Adds frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import 'semantic-ui-css/semantic.min.css';
+import './Custom.scss';
+import { Provider } from 'react-redux';
+import Home from './components/Home';
+import AboutEarth from './components/AboutEarth';
+import Upload from './components/Upload';
+import store from './store';
+import { Route, Switch } from 'react-router-dom'
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <div className="App">
+          <Switch>
+            < Route exact path="/" exact component={ Home }/>
+            < Route path="/aboutearth" exact component={ AboutEarth }/>
+            < Route path="/upload" exact component={ Upload }/>
+        </Switch>
+        </div>
+      </Provider>
+    );
+  }
+}
+
+export default App;
