73070ffd443c518141e5370f395882bd8e5fcab2
Helen Liutongco <liutongcoh@gmail.com>
2018-10-21 10:35:04 -0400

    Add audio and video hover effects

--- src/components/AudioPreview.js
+++ src/components/AudioPreview.js
@@ -2,11 +2,29 @@ import React, {Component} from 'react';
 import image from '../samples/audio.png'
 
 class AudioPreview extends Component {
+  state = {
+    hover: false
+  }
+
+  handleHover = (event) => {
+    this.setState((prevState) => {
+      return {hover: !prevState.hover}
+    }, () => {
+      if(this.state.hover){
+        this.audio.currentTime = 0
+        this.audio.play()
+      }
+      else {
+        this.audio.pause()
+      }
+    })
+  }
+
   render() {
     return (
       <div>
-        <img height="150" width="250" src={image} />
-        <audio src={this.props.url}></audio>
+        <img height="150" width="250" src={image} onMouseEnter={this.handleHover} onMouseLeave={this.handleHover}/>
+        <audio src={this.props.url} ref={(audio) => { this.audio = audio } }/>
       </div>
     )
   }
